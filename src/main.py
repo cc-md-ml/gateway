c995e55a4b0c297d06a02dc92afab790645c60f2
rayhan randi <rayhanrandi10@gmail.com>
2024-05-25 22:40:23 +0700

    feat(auth): init auth routes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from routes import global_router
+# from auth.router import router as auth_router
 
-app = FastAPI()
+
+app = FastAPI(
+    title="Cooleet Back-End API"
+)
+
+app.include_router(global_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return { "message": "Hello World" }
---
+++ src/routes.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from auth.routes import router as auth_router
+
+
+global_router = APIRouter()
+
+global_router.include_router(auth_router, tags=["Authentication"])
