001ab2ddcabd18c58c8740121ae32914c96a7cbf
Rayhan Putra Randi <rayhan.putra11@ui.ac.id>
2024-06-01 01:43:44 +0700

    feat(llm): impl initial llm integration

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .auth.routes import router as auth_router
+from .llm.routes import router as llm_router
 
 
 global_router = APIRouter()
 
 global_router.include_router(auth_router, tags=["Authentication"])
+global_router.include_router(llm_router, tags=["llm"])
