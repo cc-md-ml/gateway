bcfe49a844ee6c295722aa2a4c69f9823bb06e28
rayhan randi <rayhanrandi10@gmail.com>
2024-06-02 01:37:22 +0700

    feat(auth): init firebase auth connection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
+from src.auth import firebase
 from src.config import setup_env
 from src.routes import global_router
 
@@ -12,8 +13,18 @@ app = FastAPI(
 # return value will be cached
 setup_env()
 
+# initialize firebase authentication
+firebase.init_auth()
+
+# global app router
 app.include_router(global_router, prefix="/api/v1")
+
 
 @app.get("/")
 async def root():
     return { "message": "Hello World" }
+
+# TODO: remove dummy
+@app.get("/api/user_token")
+async def hello_user(user = Depends(firebase.get_user_token)):
+    return { "msg": "Hello, user", "uid": user['uid'] } 
